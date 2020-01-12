33291c9ff4cda65520a9ec3fa71b9e49400eb760
HasinJ <hasinchoudhuryj@gmail.com>
2020-01-12 17:20:10 -0500

    second test working

--- sumAll.js
+++ sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+  if (start<0 || end<0 || typeof(start)!=="number" || typeof(end)!=="number") {
+    return "ERROR";
+  }
+
+  let result = 0;
+  for (; start <= end; start++) {
+    result += start
+  }
+  return result;
 
 }
 
